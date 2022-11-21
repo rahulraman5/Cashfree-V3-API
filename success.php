3a5e9829a34b43e8160f8aff4d38ae5922df700d
Gaurav Mishra <gauravmishra@Gauravs-MacBook-Air.local>
2022-11-21 18:16:53 +0530

    updated cide in success file

--- success.php
+++ success.php
@@ -1,7 +1,7 @@
 <?php 
 echo "<div style='text-align:center !important;background-color:grey; margin:100px; padding: 50px;'>";
    $orderid = $_GET['order_id'];
-   echo "<h2>Your Transaction is completed successful</h2>";
+   
    echo "<p>";
    echo "<h2>Your order id   </h2>". $orderid ;
 echo "</div>"
